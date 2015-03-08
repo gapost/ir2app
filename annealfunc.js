5689ac3050441297e5d5db838903d2b7a1f650de
George Apostolopoulos <gapost@ipta.demokritos.gr>
2015-03-08 15:03:05 +0200

    Working version 0.1 with JS objects Core, Delta, TempCtrl, IrradCtrl, Anneal.

--- annealfunc.js
+++ annealfunc.js
@@ -11,14 +11,16 @@ var Anneal = {
         var R0 = data.rt.R13.mean();
 
         Core.timedPrint("Heating up ...")
-        setSampleTs(Ta)
+		Anneal.setPIDparameters(Ta);
+        TempCtrl.setSampleTs(Ta)
         wait(60*1000);
         Core.timedPrint("Annealing at " + Ta.toFixed(2) + "K, for " + ta + " min")
         wait(ta*60*1000);
 
 
         Core.timedPrint("Cooling down... ")
-        setSampleTs(Anneal.baseTemperature);
+        TempCtrl.setSampleTs(Anneal.baseTemperature);
+		Anneal.setPIDparameters(Anneal.baseTemperature);
 
         Core.timedPrint("Stabilizing at base T for "+ tw + " min")
         wait(tw*60*1000);
@@ -39,20 +41,26 @@ var Anneal = {
         print("R1 = " + R1.toFixed(4))
         print("dR = " + dR.toExponential(2))
 
-        //setR15offset(6.8766+(R1-6.92));correct the value of R15 for the change in R 6.92:value with Tcs=25K
-        //setR15offset(8.51365+0.0793);//correct the value of R15 for the change in R during irradiation
-        //setR15offset(5.704+(R1-(5.725+0.026)));	// correct the value of R15 for the change in R
+        Delta.setR15offset(6.6515+(R1-6.682)); // correct the value of R15 for the change in R 6.92:value with Tcs=23K
+		Delta.setRratio(ratio/100);
+       
     },
     cycleArray : function (Ta,ta,tw) {
         var n = Ta.length;
         for(var i=0; i<n; ++i)
         {
-            Anneal.setPIDparameters(Ta[i]);
-            Anneal.annealCycle(Ta[i],ta[i],tw[i]);
+            Anneal.cycle(Ta[i],ta[i],tw[i]);
         }
     },
     setPIDparameters : function (Ta) {
-        if (Ta <= 90) {
+        if (Ta <= 60) {
+            dev.tcs1.gain = 0.001;
+            dev.tcs2.gain = 0.001;
+            dev.tcs1.Ti = 6;
+            dev.tcs2.Ti = 6;
+            dev.tcs1.beta = 1;
+            dev.tcs2.beta = 1;
+        } else if (Ta <= 90) {
             dev.tcs1.gain = 0.0015;
             dev.tcs2.gain = 0.0015;
             dev.tcs1.Ti = 6;
@@ -121,8 +129,8 @@ var Anneal = {
         var obj = { Ta : [], ta : [], tw : [] };
         var dT, T;
 
-        dT=2.5;
-        for(T=35; T<=90; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(5);   }
+        /*dT=2.5;
+        for(T=40; T<=90; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(5);   }
         dT=3;
         for(T=93; T<=120; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(5);  }
         dT=4;
@@ -132,13 +140,14 @@ var Anneal = {
         dT=6;
         for(T=206; T<=260; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(7);  }
         dT=8;
-        for(T=268; T<=300; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(8);  }
+        for(T=268; T<=300; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(8);  }*/
         dT=10;
-        for(T=310; T<=400; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(15);  }
+        for(T=400; T<=400; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(10);  }
+		//for(T=310; T<=400; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(10);  }
         dT=15;
-        for(T=415; T<=520; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(30);  }
+        for(T=415; T<=520; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(20);  }
         dT=20;
-        for(T=540; T<=700; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(40);  }
+        for(T=540; T<=700; T+=dT) { obj.Ta.push(T); obj.ta.push(dT); obj.tw.push(30);  }
 
         return obj;
     },
